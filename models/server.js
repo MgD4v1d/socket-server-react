8a11654f7e1b924c005955ec674b60eb52b387ba
Dave <david.magania@gmail>
2022-06-24 14:51:38 -0500

    cors agregado

--- models/server.js
+++ models/server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const http = require('http');
+const express  = require('express');
+const http     = require('http');
 const socketio = require('socket.io');
-const path = require('path');
-const Sockets = require('./sockets');
+const path     = require('path');
+const Sockets  = require('./sockets');
+const cors     = require('cors');
 
 
 class Server {
@@ -22,6 +23,7 @@ class Server {
 
     middlewares(){
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+        this.app.use(cors());
     }
 
     socketsSetting(){
